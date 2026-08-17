6b72c9a523bd62a94fac4610e42b93d9db909bdd
Marlon Steiner <marlon.steiner@kit.edu>
2026-08-17 16:48:44 +0200

    Revert "Merge pull request #1 from marlon31415/extend-preprocessing-with-route". Reason: Additional data is not longer required for routing dataset creation (removed dependency).
    This reverts commit 3d318bf113f0d8f39bc61c42e60fa0a1f3e3c6a0, reversing
    changes made to 307bd2b6b5e4c2b87af1413eb87bfb8a317ef89f.

--- data_process.sh
+++ data_process.sh
@@ -1,36 +1,15 @@
 ###################################
 # User Configuration Section
 ###################################
-SPLIT="train" # specify the split to process ("train", "val", "val14", "interplan")
+NUPLAN_DATA_PATH="/path/to/nuplan-v1.1/splits/trainval" # nuplan training data path (e.g., "/data/nuplan-v1.1/splits/trainval")
+NUPLAN_MAP_PATH="/path/to/maps" # nuplan map path (e.g., "/data/nuplan-v1.1/maps")
 
-TRAIN_SET_PATH="/path/to/save/processed/data/$SPLIT" # specify a folder to save the processed training data (will be around 150GB for 1M training data)
-
+TRAIN_SET_PATH="/path/to/save/processed/data/" # specify a folder to save the processed training data (will be around 150GB for 1M training data)
 ###################################
-
-if [ "$SPLIT" = "interplan" ]; then
-    DATA_FOLDER="test"
-else
-    DATA_FOLDER="trainval"
-fi
-NUPLAN_DATA_PATH="$NUPLAN_DATA_ROOT/nuplan-v1.1/splits/$DATA_FOLDER" # nuplan training data path (e.g., "/data/nuplan-v1.1/splits/trainval")
-NUPLAN_MAP_PATH="$NUPLAN_DATA_ROOT/maps" # nuplan map path (e.g., "/data/nuplan-v1.1/maps")
-
-if [ "$SPLIT" = "train" ]; then
-    TOTAL_SCENARIOS=1000000
-elif [ "$SPLIT" = "val" ]; then
-    TOTAL_SCENARIOS=100000
-elif [ "$SPLIT" = "val14" ]; then
-    TOTAL_SCENARIOS=1118
-elif [ "$SPLIT" = "interplan" ]; then
-    TOTAL_SCENARIOS=16
-else
-    echo "Unsupported split: $SPLIT. Supported splits are: train, val, val14, interplan."
-    exit 1
-fi
 
 python data_process.py \
 --data_path $NUPLAN_DATA_PATH \
 --map_path $NUPLAN_MAP_PATH \
 --save_path $TRAIN_SET_PATH \
---total_scenarios $TOTAL_SCENARIOS \
---split $SPLIT \
+--total_scenarios 1000000 \
+
